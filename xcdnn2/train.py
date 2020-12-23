ab07f8aefd3989f94a5e2231bfebcea1892d475f
mfkasim1 <firman.kasim@gmail.com>
2020-12-23 16:25:02 +0000

    Refactoring and added logdir hparams

--- xcdnn2/train.py
+++ xcdnn2/train.py
@@ -9,6 +9,8 @@ from dqc.api.getxc import get_libxc
 from xcdnn2.dft_dataset import DFTDataset, Evaluator
 from xcdnn2.xcmodels import HybridXC
 
+FILEPATH = os.path.dirname(os.path.realpath(__file__))
+
 ###################### training module ######################
 class LitDFTXC(pl.LightningModule):
     def __init__(self, hparams: Dict):
@@ -36,7 +38,7 @@ class LitDFTXC(pl.LightningModule):
             torch.nn.Softplus(),
             torch.nn.Linear(nhid, 1, bias=False),
         ).to(torch.double)
-        model_nnlda = HybridXC(args.libxc, nnmodel)
+        model_nnlda = HybridXC(hparams["libxc"], nnmodel)
 
         weights = {
             "ie": hparams["iew"],
@@ -93,10 +95,13 @@ class LitDFTXC(pl.LightningModule):
     def add_model_specific_args(parent_parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
         # arguments to be stored in the hparams file
+        # model hyperparams
         parser.add_argument("--nhid", type=int, default=10,
                             help="The number of hidden layers")
         parser.add_argument("--libxc", type=str, default="lda_x",
                             help="Initial xc to be used")
+
+        # training hyperparams
         parser.add_argument("--ielr", type=float, default=1e-4,
                             help="Learning rate for ionization energy (chosen if there is --split_opt)")
         parser.add_argument("--aelr", type=float, default=1e-4,
@@ -111,6 +116,8 @@ class LitDFTXC(pl.LightningModule):
                             help="Weight of atomization energy")
         parser.add_argument("--dmw", type=float, default=220.0,
                             help="Weight of density matrix")
+        parser.add_argument("--max_epochs", type=int, default=1000,
+                            help="Maximum number of epochs")
         parser.add_argument("--tvset", type=int, default=2,
                             help="Training/validation set")
         parser.add_argument("--exclude_types", type=str, nargs="*", default=[],
@@ -119,43 +126,31 @@ class LitDFTXC(pl.LightningModule):
                             help="Flag to split optimizer based on the dataset type")
         return parser
 
-def get_exp_version(version: Optional[str]) -> Optional[str]:
-    if version is not None:
-        if version.isdigit():
-            return "version_%d" % int(version)
-    return version
-
-if __name__ == "__main__":
-    from xcdnn2.utils import subs_present
-
-    torch.manual_seed(123)
-
-    # parsing the hyperparams
+def get_program_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("--record", action="store_const", default=False, const=True,
                         help="Flag to record the progress")
     parser.add_argument("--version", type=str,
                         help="The training version, if exists, then resume the training")
-    parser = LitDFTXC.add_model_specific_args(parser)
-    # parser = pl.Trainer.add_argparse_args(parser)
-    args = parser.parse_args()
+    parser.add_argument("--logdir", type=str, default="logs",
+                        help="The log directory relative to this file's path")
+    return parser
 
-    # putting all the hyperparameters in a dictionary
-    hparams = vars(args)
-
-    # create the lightning module
-    plsystem = LitDFTXC(hparams)
+def get_datasets(hparams: Dict):
+    from xcdnn2.utils import subs_present
+    # load the datasets and returns the dataloader for training and validation
 
     # load the dataset and split into train and val
     dset = DFTDataset()
-    if args.tvset == 1:
+    tvset = hparams["tvset"]
+    if tvset == 1:
         # train_atoms = ["H", "He", "Li", "Be", "B", "C"]
         val_atoms = ["N", "O", "F", "Ne"]
-    elif args.tvset == 2:  # randomly selected
+    elif tvset == 2:  # randomly selected
         # train_atoms = ["H", "Li", "B", "C", "O", "Ne"]
         val_atoms = ["He", "Be", "N", "F"]
 
-    general_filter = lambda obj: obj["type"] not in args.exclude_types
+    general_filter = lambda obj: obj["type"] not in hparams["exclude_types"]
     all_idxs = dset.get_indices(general_filter)
     val_filter = lambda obj: subs_present(val_atoms, obj["name"].split()[-1]) and general_filter(obj)
     val_idxs = dset.get_indices(val_filter)
@@ -172,18 +167,21 @@ if __name__ == "__main__":
     dset_val = Subset(dset, val_idxs)
     dloader_train = DataLoader(dset_train, batch_size=None)
     dloader_val = DataLoader(dset_val, batch_size=None)
+    return dloader_train, dloader_val
 
+def get_trainer(hparams: Dict):
     # setup the trainer
     trainer_kwargs = {
         "logger": False,
         "checkpoint_callback": False,
         "num_sanity_val_steps": 0,
-        "gradient_clip_val": args.clipval,
+        "gradient_clip_val": hparams["clipval"],
         "automatic_optimization": False,
+        "max_epochs": hparams["max_epochs"],
     }
-    logdir = "logs/"
-    version = get_exp_version(args.version)
-    if args.record:
+    logdir = os.path.join(FILEPATH, hparams["logdir"])
+    version = get_exp_version(hparams["version"])
+    if hparams["record"]:
         # set up the logger
         tb_logger = pl.loggers.TensorBoardLogger(logdir, version=version)
         chkpt_val = ModelCheckpoint(monitor="val_loss", save_top_k=4, save_last=True)
@@ -199,6 +197,33 @@ if __name__ == "__main__":
             trainer_kwargs["resume_from_checkpoint"] = fpath
 
     trainer = pl.Trainer(**trainer_kwargs)
+    return trainer
+
+def get_exp_version(version: Optional[str]) -> Optional[str]:
+    # get the experiment version based on the input from the user's hparams
+    if version is not None:
+        if version.isdigit():
+            return "version_%d" % int(version)
+    return version
+
+def run_training(hparams: Dict):
+    # create the lightning module and the datasets
+    plsystem = LitDFTXC(hparams)
+    dloader_train, dloader_val = get_datasets(hparams)
+    trainer = get_trainer(hparams)
     trainer.fit(plsystem,
                 train_dataloader=dloader_train,
                 val_dataloaders=dloader_val)
+
+if __name__ == "__main__":
+    torch.manual_seed(123)
+
+    # parsing the hyperparams
+    parser = get_program_argparse()
+    parser = LitDFTXC.add_model_specific_args(parser)
+    # parser = pl.Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+
+    # putting all the hyperparameters in a dictionary
+    hparams = vars(args)
+    run_training(hparams)
